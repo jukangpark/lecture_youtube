7d74910b96e9063e38334e187c3d58d434cd0a27
jukangpark <skyxxx9339@naver.com>
2022-11-06 20:23:49 +0900

    변수의 선언과 할당

--- variable.js
+++ variable.js
@@ -1,4 +1,12 @@
-const wallet = 1000; // 대입 연산자
-wallet + 230000000; // evaluation 계산 '평가'
+// 변수란 무엇인가
+// 3300000000
+
+let wallet = 1000;
+
+console.log(wallet + 3300000000); // 계산(cpu)
+
+wallet = wallet + 3300000000; // 저장(memory)
 
 console.log(wallet);
+
+// 변수 어떤 값을 저장하기위해 확보된 메모리 공간을 말해요
